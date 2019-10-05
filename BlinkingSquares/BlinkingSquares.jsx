e26bfdd3953fa779cd996fe9ebadf7331be968ca
Vince Feil <vfeil@msudenver.edu>
2019-10-05 10:39:25 -0600

    Added comments

--- BlinkingSquares/BlinkingSquares.jsx
+++ BlinkingSquares/BlinkingSquares.jsx
@@ -1,4 +1,8 @@
+//using the for loop to plot squares instead of plotting each diode
+//individualy
 basic.forever(function () {
+    //this group plots the outside square pauses for 1/2 second and
+    //clears the screen
     for (let index = 0; index <= 4; index++) {
         led.plot(0, index)
     }
@@ -15,7 +19,9 @@ basic.forever(function () {
     basic.clearScreen()
     basic.pause(500)
 
-    for (let index5 = 1; index5 <= 3; index5++) {
+    //this group plots the middle square pauses for 1/2 second and
+    //clears the screen
+        for (let index5 = 1; index5 <= 3; index5++) {
         led.plot(1, index5)
     }
     for (let index6 = 2; index6 <= 3; index6++) {
@@ -30,11 +36,16 @@ basic.forever(function () {
     basic.pause(500)
     basic.clearScreen()
     basic.pause(500)
+
+    //this plots the inner led pauses for 1/2 second then clears the
+    //screen
     led.plot(2, 2)
     basic.pause(500)
     basic.clearScreen()
     basic.pause(500)
 
+    //this repeats the second square to make it appear the squares go
+    //in and then out
     for (let index5 = 1; index5 <= 3; index5++) {
         led.plot(1, index5)
     }
