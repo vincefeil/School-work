9bb3feece765e9b1e9d701b1262421823eae52c4
Vince Feil <vfeil@msudenver.edu>
2019-10-05 18:44:06 -0600

    added a functions file

--- Screen Savers/functions.jsx
+++ Screen Savers/functions.jsx
@@ -1,101 +1,77 @@
 let motion = 0
-// set up variables for dot1
-let xdot1 = 0
-let ydot1 = 4
-// set up variables for dot2
-let xdot2 = 4
-let ydot2 = 4
-// initial display of variables
-led.plot(xdot1, ydot1)
-led.plot(xdot2, ydot2)
-
-input.onButtonPressed(Button.B, function () {
-    motion = 0
-
-})
-
+let blink = 0
 input.onButtonPressed(Button.A, function () {
-    motion = 1
+    blinkingSquare()
 })
-basic.forever(function () {
-
+function blinkingSquare () {
+    motion = 1
     for (let index1t = 0; index1t <= 3; index1t++) {
-
         if (motion == 1) {
-
-
             for (let index = 0; index <= 4; index++) {
                 led.plot(0, index)
             }
             for (let index2 = 1; index2 <= 4; index2++) {
                 led.plot(index2, 4)
             }
-            for (let index3 = 3; index3 >= 0; index3 += -1) {
+for (let index3 = 3; index3 >= 0; index3 += -1) {
                 led.plot(4, index3)
             }
-            for (let index4 = 3; index4 >= 0; index4 += -1) {
+for (let index4 = 3; index4 >= 0; index4 += -1) {
                 led.plot(index4, 0)
             }
-            basic.pause(500)
+basic.pause(500)
             basic.clearScreen()
             basic.pause(500)
-
             for (let index5 = 1; index5 <= 3; index5++) {
                 led.plot(1, index5)
             }
-            for (let index6 = 2; index6 <= 3; index6++) {
+for (let index6 = 2; index6 <= 3; index6++) {
                 led.plot(index6, 3)
             }
-            for (let index7 = 3; index7 >= 1; index7 += -1) {
+for (let index7 = 3; index7 >= 1; index7 += -1) {
                 led.plot(3, index7)
             }
-            for (let index8 = 3; index8 >= 1; index8 += -1) {
+for (let index8 = 3; index8 >= 1; index8 += -1) {
                 led.plot(index8, 1)
             }
-            basic.pause(500)
+basic.pause(500)
             basic.clearScreen()
             basic.pause(500)
             led.plot(2, 2)
             basic.pause(500)
             basic.clearScreen()
             basic.pause(500)
-
-            for (let index5 = 1; index5 <= 3; index5++) {
-                led.plot(1, index5)
+            for (let index52 = 1; index52 <= 3; index52++) {
+                led.plot(1, index52)
             }
-            for (let index6 = 2; index6 <= 3; index6++) {
-                led.plot(index6, 3)
+for (let index62 = 2; index62 <= 3; index62++) {
+                led.plot(index62, 3)
             }
-            for (let index7 = 3; index7 >= 1; index7 += -1) {
-                led.plot(3, index7)
+for (let index72 = 3; index72 >= 1; index72 += -1) {
+                led.plot(3, index72)
             }
-            for (let index8 = 3; index8 >= 1; index8 += -1) {
-                led.plot(index8, 1)
+for (let index82 = 3; index82 >= 1; index82 += -1) {
+                led.plot(index82, 1)
             }
-            basic.pause(500)
+basic.pause(500)
             basic.clearScreen()
             basic.pause(500)
-
-            for (let index = 0; index <= 4; index++) {
-                led.plot(0, index)
+            for (let index9 = 0; index9 <= 4; index9++) {
+                led.plot(0, index9)
             }
-            for (let index2 = 1; index2 <= 4; index2++) {
-                led.plot(index2, 4)
+            for (let index22 = 1; index22 <= 4; index22++) {
+                led.plot(index22, 4)
             }
-            for (let index3 = 3; index3 >= 0; index3 += -1) {
-                led.plot(4, index3)
+for (let index32 = 3; index32 >= 0; index32 += -1) {
+                led.plot(4, index32)
             }
-            for (let index4 = 3; index4 >= 0; index4 += -1) {
-                led.plot(index4, 0)
+for (let index42 = 3; index42 >= 0; index42 += -1) {
+                led.plot(index42, 0)
             }
-            basic.pause(500)
+basic.pause(500)
             basic.clearScreen()
             basic.pause(500)
-
-
-
-            let blink = 100
-
+            blink = 100
             led.plotBrightness(0, 0, Math.randomRange(0, 225))
             basic.pause(blink)
             led.plotBrightness(1, 0, Math.randomRange(0, 225))
@@ -146,61 +122,10 @@ basic.forever(function () {
             basic.pause(blink)
             led.plotBrightness(4, 4, Math.randomRange(0, 225))
             basic.pause(blink)
-            basic.clearScreen()
-
-            if (motion = 1) {
-                if (xdot1 == 0) {
-
-                    while (xdot1 < 4) {
-                        led.unplot(xdot1, ydot1)
-                        xdot1 += 1
-                        ydot1 += -1
-                        // initial display of variables
-                        led.plot(xdot1, ydot1)
-                        basic.pause(200)
-                    }
-                }
-                if (xdot1 == 4) {
-                    while (xdot1 > 0) {
-                        led.unplot(xdot1, ydot1)
-                        xdot1 += -1
-                        ydot1 += 1
-                        // initial display of variables
-                        led.plot(xdot1, ydot1)
-                        basic.pause(200)
-                    }
-                }
-                if (xdot2 == 0) {
-
-                    while (xdot2 < 4) {
-                        led.unplot(xdot2, ydot2)
-                        xdot2 += 1
-                        ydot2 += 1
-                        // initial display of variables
-                        led.plot(xdot2, ydot2)
-                        basic.pause(200)
-                    }
-                }
-                if (xdot2 == 4) {
-                    while (xdot2 > 0) {
-                        led.unplot(xdot2, ydot2)
-                        xdot2 += -1
-                        ydot2 += -1
-                        // initial display of variables
-                        led.plot(xdot2, ydot2)
-                        basic.pause(200)
-                    }
-                }
-            }
-
         }
         basic.clearScreen()
-
-
     }
-
-
-
-
+}
+input.onButtonPressed(Button.B, function () {
+    motion = 0
 })
- 
