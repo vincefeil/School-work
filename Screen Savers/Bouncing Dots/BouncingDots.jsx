bacb8022d0fc593fb99d7874533e2d2be1903608
Vince Feil <vfeil@msudenver.edu>
2019-10-06 07:36:18 -0600

    Added function

--- Screen Savers/Bouncing Dots/BouncingDots.jsx
+++ Screen Savers/Bouncing Dots/BouncingDots.jsx
@@ -1,28 +1,6 @@
-input.onButtonPressed(Button.A, function () {
-    if (A) {
-        A = false
-    } else {
-        A = true
-    }
-})
-let A = false
-let B = false
-let AB = false
-A = false
-// set up variables for dot1
-let xdot1 = 0
-let ydot1 = 4
-// set up variables for dot2
-let xdot2 = 4
-let ydot2 = 4
-// initial display of variables
-led.plot(xdot1, ydot1)
-led.plot(xdot2, ydot2)
-// sets up the funtions to run in the background
-basic.forever(function () {
-    while (A) {
+function bouncingDots () {
+    while (dotTime > 0) {
         if (xdot1 == 0) {
-            
             while (xdot1 < 4) {
                 led.unplot(xdot1, ydot1)
                 xdot1 += 1
@@ -43,7 +21,6 @@ basic.forever(function () {
             }
         }
         if (xdot2 == 0) {
-
             while (xdot2 < 4) {
                 led.unplot(xdot2, ydot2)
                 xdot2 += 1
@@ -63,5 +40,20 @@ basic.forever(function () {
                 basic.pause(200)
             }
         }
-    }   
-})
+        dotTime += -500
+    }
+}
+
+let xdot1 = 0
+let dotTime = 0
+let ydot2 = 0
+let xdot2 = 0
+let ydot1 = 0
+ydot1 = 4
+// set up variables for dot2
+xdot2 = 4
+ydot2 = 4
+dotTime = 2000
+// initial display of variables
+led.plot(xdot1, ydot1)
+led.plot(xdot2, ydot2)
