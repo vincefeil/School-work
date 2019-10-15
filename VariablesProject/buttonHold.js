9b5c8fbbc0307d422545785cb0ab479e28dc66c0
Vincent Feil <vfeil@melco.local>
2019-10-14 18:59:53 -0600

    Added comments

--- VariablesProject/buttonHold.js
+++ VariablesProject/buttonHold.js
@@ -1,4 +1,7 @@
-//function is for demonstration purposes
+//function is for demonstration purposes. when called it displays
+//F and checks the the variable stringTime. if it is greater than 0 
+//it subtracts 1 and checks again. when it reaches 0 it exits and
+//and returns to the forever loop
 function showString () {
     while (stringTime > 0) {
         basic.showString("F")
@@ -13,9 +16,15 @@ let time = 2
 stringTime = 5
 
 basic.forever(function () {
+    //while tests for button A pressed and hold. If true, it sets the variable
+    //buttonPressed to 1. It shows the value of variable.
+
     while (input.buttonIsPressed(Button.A)) {
         buttonPressed = 1
         basic.showString("" + buttonPressed)
+        //if function test for time variable to = 0. if it doesn't
+        //it will subtract 1 and check again. once it reaches 0 it
+        //calls the function showString.
         if (time > 0) {
             basic.showNumber(time)
             time += -1
@@ -24,6 +33,8 @@ basic.forever(function () {
             }
         }
     }
+    //sets variable to 0 and displays the value of the variable until
+    //the A button is pressed and held.
     buttonPressed = 0
     basic.showString("" + buttonPressed)
 })
