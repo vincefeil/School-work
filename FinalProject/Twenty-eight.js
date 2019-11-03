5a1033594cb4dadded900b2959a10b6422bc9213
Vince Feil <vfeil@msudenver.edu>
2019-11-03 10:54:39 -0700

    added comments

--- FinalProject/Twenty-eight.js
+++ FinalProject/Twenty-eight.js
@@ -2,22 +2,25 @@
 input.onButtonPressed(Button.A, function () {
     arrowsI()
 })
-//the function turns on and off select
+//the function turns on and off select making it look like arrows are crossing the screen
 function arrowsI() {
-    while (ArrowTimeI > 0) {
-        if (F == 1) {
-            for (let xindex00 = 0; xindex00 <= 4; xindex00++) {
-                led.plot(xindex00, 0)
-                led.plot(xindex11, 1)
+    while (ArrowTimeI > 0) { //while function is used as a timer when the variable ArrowTimeI reaches 0 it exits the function
+        
+            for (let xindex00 = 0; xindex00 <= 4; xindex00++) { //using the for loop to set up limits on the top row of the LEDs. This 
+                                                                //makes it start over on the left when it reaches the right side of display
+                led.plot(xindex00, 0)//using plot and Variables to move the arrows accross the screen. The Y value is constant position
+                led.plot(xindex11, 1)//is determined by the X variable
                 led.plot(xindex22, 2)
                 led.plot(xindex33, 3)
                 led.plot(xindex44, 4)
-                xindex11 += 1
+                xindex11 += 1// adding 1 to the x variable makes the dots change posotion from left to right
                 xindex22 += 1
                 xindex33 += 1
                 xindex44 += 1
-                basic.pause(200)
-                basic.clearScreen()
+                basic.pause(200)//pause to let user see the results
+                basic.clearScreen()//clear screen removes the previous plots. if not done the screen would fill up and not look like
+                                    //moving arrows
+                //these if statements track the movement of the four lower rows and reset the variables to 0 when the reach the last column
                 if (xindex11 > 4) {
                     xindex11 = 0
                 }
@@ -31,15 +34,16 @@ function arrowsI() {
                     xindex44 = 0
                 }
             }
-            ArrowTimeI += -500
-        }
+            ArrowTimeI += -500//subtracts 500ms from the timing variable
+        
     }
-    basic.clearScreen()
+    basic.clearScreen()//clears the screen at the end of the running time
+    //resets the variables so the function can start again when the A button is pressed.
     xindex11 = 1
     xindex22 = 2
     xindex33 = 1
     ArrowTimeI = 5000
-    F = 1
+    
 }
 
 //turn three LEDs on and off in sequence
@@ -49,28 +53,27 @@ pins.digitalWritePin(DigitalPin.P8, 0)
 pins.digitalWritePin(DigitalPin.P16, 0)
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P12, 1)//turn on first LED
-    basic.pause(1000)//pause for 1/2 second
+    basic.pause(500)//pause for 1/2 second
     pins.digitalWritePin(DigitalPin.P8, 1)//turn on second LED
-    basic.pause(1000)
+    basic.pause(500)
     pins.digitalWritePin(DigitalPin.P16, 1)//turn on third LED
-    basic.pause(1000)
+    basic.pause(500)
     pins.digitalWritePin(DigitalPin.P16, 0)//turn off third LED
-    basic.pause(1000)
+    basic.pause(500)
     pins.digitalWritePin(DigitalPin.P8, 0)//turn off second LED
-    basic.pause(1000)
+    basic.pause(500)
     pins.digitalWritePin(DigitalPin.P12, 0)//turn off first LED
-    basic.pause(1000)
+    basic.pause(500)
 
 })
-let xindex44 = 0
-let F = 0
-let ArrowTimeI = 0
-let xindex33 = 0
-let xindex22 = 0
-let xindex11 = 0
-let xindex022 = 0
+let xindex44: number = 0
+let ArrowTimeI: number = 0
+let xindex33: number = 0
+let xindex22: number = 0
+let xindex11: number = 0
+let xindex022: number = 0
 xindex11 = 1
 xindex22 = 2
 xindex33 = 1
 ArrowTimeI = 5000
-F = 1
+
