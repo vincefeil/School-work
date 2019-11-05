f656e5594daa74738759d9b19a42ddc801b2ddcf
vincefeil <54681226+vincefeil@users.noreply.github.com>
2019-11-05 06:34:52 -0700

    Modified Code for input

--- MoistureSensor/autoCalibtrate.js
+++ MoistureSensor/autoCalibtrate.js
@@ -1,25 +1,55 @@
-input.onButtonPressed(Button.A, function () {
-    low1 = sensorReading
-    basic.pause(100)
-    low2 = sensorReading
-    basic.pause(100)
-    low3 = sensorReading
-    basic.pause(100)
-    low = [low1, low2, low3]
-    lowDone = true
-})
-let low: number[] = []
-let low3 = 0
-let low2 = 0
-let sensorReading = 0
-let low1 = 0
-let lowDone = false
-lowDone = false
-basic.forever(function () {
-    if (lowDone) {
-        basic.clearScreen()
-    } else {
-        basic.showIcon(IconNames.Heart)
+function runnig() {
+    while (Cal1 == 3) {
+        basic.showString("Running")
     }
-    sensorReading = pins.analogReadPin(AnalogPin.P0)
+}
+input.onButtonPressed(Button.B, function () {
+    Cal1 = 0
 })
+function setup() {
+    while (Cal1 < 3) {
+        if (Cal1 == 0) {
+            if (input.buttonIsPressed(Button.A)) {
+                Cal1 = 1
+                basic.showNumber(Cal1)
+                basic.pause(500)
+                basic.clearScreen()
+            }
+            basic.showIcon(IconNames.Sword)
+        }
+        if (Cal1 == 1) {
+            if (input.buttonIsPressed(Button.A)) {
+                Cal1 = 2
+                basic.showNumber(Cal1)
+                basic.pause(500)
+                basic.clearScreen()
+            }
+            basic.showIcon(IconNames.Sword)
+        }
+        if (Cal1 == 2) {
+            if (input.buttonIsPressed(Button.A)) {
+                Cal1 = 3
+                basic.showNumber(Cal1)
+                basic.pause(500)
+                basic.clearScreen()
+            }
+        }
+        if (Cal1 == 3) {
+            if (input.buttonIsPressed(Button.A)) {
+                Cal1 = 3
+                basic.showNumber(Cal1)
+                basic.pause(500)
+                basic.clearScreen()
+            }
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+    }
+}
+let Cal1 = 0
+setup()
