4d317e76e16bef1616be542f5d34428c4862ccc7
vincefeil <54681226+vincefeil@users.noreply.github.com>
2019-11-05 19:13:12 -0700

    Update working.js
    Now has full functionality of moisture sensor aplication

--- MoistureSensor/working.js
+++ MoistureSensor/working.js
@@ -1,21 +1,23 @@
 function Average() {
-    while (Cal1 == 6) {
-        //Adds the three low readings taken from calibration and divides by three to get an average
-        lowAverage = (lowReading1 + (lowReading2 + lowReading3)) / 3
-        basic.showNumber(lowAverage)//Shows the value lowAverage
-        serial.writeNumber(lowAverage)//Serial display of lowAverage
-        //hiReading1 = 1
-        //hiReading2 = 1
-        //hiReading3 = 1
-        //Adds the three hi readings taken from calibration and divides by three to get an average
-        hiAverage = (hiReading1 + (hiReading2 + hiReading3)) / 3
-        basic.showNumber(hiAverage)//Shows the value hiAverage
-        serial.writeNumber(hiAverage)//Serial display of hiAverage
+    //while (Cal1 == 6) {
+    //Adds the three low readings taken from calibration and divides by three to get an average
+    lowAverage = (lowReading1 + (lowReading2 + lowReading3)) / 3
+    //basic.showNumber(lowAverage)//Shows the value lowAverage
+    serial.writeNumber(lowAverage)//Serial display of lowAverage
+    //hiReading1 = 1
+    //hiReading2 = 1
+    //hiReading3 = 1
+    //Adds the three hi readings taken from calibration and divides by three to get an average
+    hiAverage = (hiReading1 + (hiReading2 + hiReading3)) / 3
+    //basic.showNumber(hiAverage)//Shows the value hiAverage
+    serial.writeNumber(hiAverage)//Serial display of hiAverage
+    //basic.clearScreen()
 
-    }
 }
+//}
 input.onButtonPressed(Button.B, function () {
     Cal1 = 0
+    setup()
 })
 function setup() {
     while (Cal1 < 6) {
@@ -23,7 +25,7 @@ function setup() {
             if (input.buttonIsPressed(Button.A)) {
                 Cal1 = 1
                 pins.digitalWritePin(DigitalPin.P12, 1)
-                lowReading1 = pins.analogReadPin(AnalogPin.P0)    
+                lowReading1 = pins.analogReadPin(AnalogPin.P0)
                 basic.showNumber(1)
                 pins.digitalWritePin(DigitalPin.P12, 0)
                 basic.pause(500)
@@ -133,6 +135,72 @@ function setup() {
         }
     }
 }
+// bar gragh display function
+function display() {
+    // if takes the value in moisture_reading, remaps it
+    // to 0-4, rounds to a whole number then compairs it
+    // to an integer and displays the corresponding bar
+    // graph if it equals 0 it clears the screen
+    if (Math.round(Math.map(moisture_reading, lowAverage, hiAverage, 0, 4)) == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            `)
+    } else if (Math.round(Math.map(moisture_reading, lowAverage, hiAverage, 0, 4)) == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . # # .
+            . # # # .
+            `)
+    } else if (Math.round(Math.map(moisture_reading, lowAverage, hiAverage, 0, 4)) == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            `)
+    } else if (Math.round(Math.map(moisture_reading, lowAverage, hiAverage, 0, 4)) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+}
+// on button A pressed and released turns on pin 12
+// for 1 second which turns on the sensor. pin 0 reads
+// the sensor and writes the data into the variable
+// moisture_reading. then pin 12 is turned off
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    basic.pause(1000)
+    moisture_reading = pins.analogReadPin(AnalogPin.P0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    display()
+})
+// every 15 seconds it turns on pin 12 for 1 second
+// which turns on the sensor. pin 0 reads the sensor
+// and writes the data into the variable
+// moisture_reading. then pin 12 is turned off
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    basic.pause(1000)
+    moisture_reading = pins.analogReadPin(AnalogPin.P0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    display()
+    basic.pause(15000)
+    serial.writeNumber(moisture_reading)
+})
 let Cal1 = 0
 let lowReading3: number = 0
 let lowReading2: number = 0
@@ -140,7 +208,8 @@ let lowReading1: number = 0
 let hiReading3: number = 0
 let hiReading2: number = 0
 let hiReading1: number = 0
-let hiAverage = 0
-let lowAverage = 0
+let hiAverage: number = 0
+let lowAverage: number = 0
+let moisture_reading: number = 0
 setup()
 
