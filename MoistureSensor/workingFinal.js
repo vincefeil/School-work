82d683e8b46173a9abaaef7041c1a47076a6620d
vincefeil <54681226+vincefeil@users.noreply.github.com>
2019-11-08 11:15:12 -0700

    Code clean up and commenting

--- MoistureSensor/workingFinal.js
+++ MoistureSensor/workingFinal.js
@@ -1,36 +1,30 @@
 function Average() {
-    //while (Cal1 == 6) {
-    //Adds the three low readings taken from calibration and divides by three to get an average
+    
+    //Adds the three low readings taken from setup and divides by three to get an average
     lowAverage = (lowReading1 + (lowReading2 + lowReading3)) / 3
-    //basic.showNumber(lowAverage)//Shows the value lowAverage
-    //serial.writeNumber(lowAverage)//Serial display of lowAverage
-    //hiReading1 = 1
-    //hiReading2 = 1
-    //hiReading3 = 1
-    //Adds the three hi readings taken from calibration and divides by three to get an average
+    //Adds the three hi readings taken from setup and divides by three to get an average
     hiAverage = (hiReading1 + (hiReading2 + hiReading3)) / 3
-    //basic.showNumber(hiAverage)//Shows the value hiAverage
-    //serial.writeNumber(hiAverage)//Serial display of hiAverage
-    //basic.clearScreen()
-
 }
-//}
+
+//Pressing A and B together restarts calibration
 input.onButtonPressed(Button.AB, function () {
     Cal1 = 0
     setup()
 })
+//setup function has the user take three low readings and three high readings and places them in six veriables
 function setup() {
-    while (Cal1 < 6) {
-        if (Cal1 == 0) {
-            if (input.buttonIsPressed(Button.A)) {
-                Cal1 = 1
-                pins.digitalWritePin(DigitalPin.P12, 1)
-                lowReading1 = pins.analogReadPin(AnalogPin.P0)
-                basic.showNumber(1)
-                pins.digitalWritePin(DigitalPin.P12, 0)
-                basic.pause(1000)
-                basic.clearScreen()
+    while (Cal1 < 6) {//Cal1 tracks the setup routine. if less than 6 it is not complete and the fuction continues to run
+        if (Cal1 == 0) {//executes the following if Cal1 = 0
+            if (input.buttonIsPressed(Button.A)) {//executes the following if button A is pressed
+                Cal1 = 1//changes Cal1 to = 1
+                pins.digitalWritePin(DigitalPin.P12, 1)//Turns on the moisture sensor connected to pin 12
+                lowReading1 = pins.analogReadPin(AnalogPin.P0)//takes a reading from pin o and stores it in lowReading1 variable.
+                basic.showNumber(1)//shows the number 1 to tell the user it has taken the first low reading
+                pins.digitalWritePin(DigitalPin.P12, 0)//turns off the moisture sensor
+                basic.pause(1000)//pauses for 1 second so the user can see the display
+                basic.clearScreen()//clears the screen for the next display
             }
+            //shows the down arrow telling the user it is waiting to take a low reading. User must press A to activate.
             basic.showLeds(`
                 . . # . .
                 . . # . .
@@ -39,16 +33,17 @@ function setup() {
                 . . # . .
                 `)
         }
-        if (Cal1 == 1) {
-            if (input.buttonIsPressed(Button.A)) {
-                Cal1 = 2
-                pins.digitalWritePin(DigitalPin.P12, 1)
-                lowReading2 = pins.analogReadPin(AnalogPin.P0)
-                basic.showNumber(2)
-                pins.digitalWritePin(DigitalPin.P12, 0)
-                basic.pause(1000)
-                basic.clearScreen()
+        if (Cal1 == 1) {//executes the following if Cal1 = 1
+            if (input.buttonIsPressed(Button.A)) {//executes the following if button A is pressed
+                Cal1 = 2//changes Cal1 to = 2
+                pins.digitalWritePin(DigitalPin.P12, 1)//Turns on the moisture sensor connected to pin 12
+                lowReading2 = pins.analogReadPin(AnalogPin.P0)//takes a reading from pin o and stores it in lowReading2 variable.
+                basic.showNumber(2)//shows the number 2 to tell the user it has taken the second low reading
+                pins.digitalWritePin(DigitalPin.P12, 0)//turns off the moisture sensor
+                basic.pause(1000)//pauses for 1 second so the user can see the display
+                basic.clearScreen()//clears the screen for the next display
             }
+            //shows the down arrow telling the user it is waiting to take a low reading. User must press A to activate.
             basic.showLeds(`
                 . . # . .
                 . . # . .
@@ -57,16 +52,17 @@ function setup() {
                 . . # . .
                 `)
         }
-        if (Cal1 == 2) {
-            if (input.buttonIsPressed(Button.A)) {
-                Cal1 = 3
-                pins.digitalWritePin(DigitalPin.P12, 1)
-                lowReading3 = pins.analogReadPin(AnalogPin.P0)
-                basic.showNumber(3)
-                pins.digitalWritePin(DigitalPin.P12, 0)
-                basic.pause(1000)
-                basic.clearScreen()
+        if (Cal1 == 2) { //executes the following if Cal1 = 2
+            if (input.buttonIsPressed(Button.A)) { //executes the following if button A is pressed
+                Cal1 = 3 //changes Cal1 to = 3
+                pins.digitalWritePin(DigitalPin.P12, 1) //Turns on the moisture sensor connected to pin 12
+                lowReading3 = pins.analogReadPin(AnalogPin.P0) //takes a reading from pin o and stores it in lowReading3 variable.
+                basic.showNumber(3) //shows the number 3 to tell the user it has taken the third low reading
+                pins.digitalWritePin(DigitalPin.P12, 0) //turns off the moisture sensor
+                basic.pause(1000) //pauses for 1 second so the user can see the display
+                basic.clearScreen() //clears the screen for the next display
             }
+            //shows the down arrow telling the user it is waiting to take a low reading. User must press A to activate.
             basic.showLeds(`
                 . . # . .
                 . . # . .
@@ -75,16 +71,17 @@ function setup() {
                 . . # . .
                 `)
         }
-        if (Cal1 == 3) {
-            if (input.buttonIsPressed(Button.A)) {
-                Cal1 = 4
-                pins.digitalWritePin(DigitalPin.P12, 1)
-                hiReading1 = pins.analogReadPin(AnalogPin.P0)
-                basic.showNumber(1)
-                pins.digitalWritePin(DigitalPin.P12, 0)
-                basic.pause(1000)
-                basic.clearScreen()
+        if (Cal1 == 3) { //executes the following if Cal1 = 3
+            if (input.buttonIsPressed(Button.A)) { //executes the following if button A is pressed
+                Cal1 = 4 //changes Cal1 to = 4
+                pins.digitalWritePin(DigitalPin.P12, 1) //Turns on the moisture sensor connected to pin 12
+                hiReading1 = pins.analogReadPin(AnalogPin.P0) //takes a reading from pin o and stores it in hiReading1 variable.
+                basic.showNumber(1) //shows the number 1 to tell the user it has taken the first high reading
+                pins.digitalWritePin(DigitalPin.P12, 0) //turns off the moisture sensor
+                basic.pause(1000) //pauses for 1 second so the user can see the display
+                basic.clearScreen() //clears the screen for the next display
             }
+            //shows the up arrow telling the user it is waiting to take a high reading. User must press A to activate.
             basic.showLeds(`
                 . . # . .
                 . # # # .
@@ -93,16 +90,17 @@ function setup() {
                 . . # . .
                 `)
         }
-        if (Cal1 == 4) {
-            if (input.buttonIsPressed(Button.A)) {
-                Cal1 = 5
-                pins.digitalWritePin(DigitalPin.P12, 1)
-                hiReading2 = pins.analogReadPin(AnalogPin.P0)
-                basic.showNumber(2)
-                pins.digitalWritePin(DigitalPin.P12, 0)
-                basic.pause(1000)
-                basic.clearScreen()
+        if (Cal1 == 4) { //executes the following if Cal1 = 4
+            if (input.buttonIsPressed(Button.A)) { //executes the following if button A is pressed
+                Cal1 = 5 //changes Cal1 to = 5
+                pins.digitalWritePin(DigitalPin.P12, 1) //Turns on the moisture sensor connected to pin 12
+                hiReading2 = pins.analogReadPin(AnalogPin.P0) //takes a reading from pin o and stores it in hiReading2 variable.
+                basic.showNumber(2) //shows the number 2 to tell the user it has taken the second high reading
+                pins.digitalWritePin(DigitalPin.P12, 0) //turns off the moisture sensor
+                basic.pause(1000) //pauses for 1 second so the user can see the display
+                basic.clearScreen() //clears the screen for the next display
             }
+            //shows the up arrow telling the user it is waiting to take a high reading. User must press A to activate.
             basic.showLeds(`
                 . . # . .
                 . # # # .
@@ -111,16 +109,17 @@ function setup() {
                 . . # . .
                 `)
         }
-        if (Cal1 == 5) {
-            if (input.buttonIsPressed(Button.A)) {
-                Cal1 = 6
-                pins.digitalWritePin(DigitalPin.P12, 1)
-                hiReading3 = pins.analogReadPin(AnalogPin.P0)
-                basic.showNumber(3)
-                pins.digitalWritePin(DigitalPin.P12, 0)
-                basic.pause(1000)
-                basic.clearScreen()
+        if (Cal1 == 5) { //executes the following if Cal1 = 5
+            if (input.buttonIsPressed(Button.A)) { //executes the following if button A is pressed
+                Cal1 = 6 //changes Cal1 to = 6
+                pins.digitalWritePin(DigitalPin.P12, 1) //Turns on the moisture sensor connected to pin 12
+                hiReading3 = pins.analogReadPin(AnalogPin.P0) //takes a reading from pin o and stores it in hiReading2 variable.
+                basic.showNumber(3) //shows the number 3 to tell the user it has taken the third high reading
+                pins.digitalWritePin(DigitalPin.P12, 0) //turns off the moisture sensor
+                basic.pause(1000) //pauses for 1 second so the user can see the display
+                basic.clearScreen()//clears the screen for the next display
             }
+            //shows the up arrow telling the user it is waiting to take a high reading. User must press A to activate.
             basic.showLeds(`
                 . . # . .
                 . # # # .
@@ -129,9 +128,9 @@ function setup() {
                 . . # . .
                 `)
         }
-        if (Cal1 == 6) {
-            basic.showString("Calibration Done")
-            Average()
+        if (Cal1 == 6) { //executes the following if Cal1 = 6
+            basic.showString("Calibration Done") //Displays Calibration done 
+            Average() //Calls the averaging function
         }
     }
 }
@@ -180,7 +179,7 @@ function display() {
 // on button A pressed and released turns on pin 12
 // for 1 second which turns on the sensor. pin 0 reads
 // the sensor and writes the data into the variable
-// moisture_reading. then pin 12 is turned off
+// moisture_reading. then pin 12 is turned off and the display function is called
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P12, 1)
@@ -192,7 +191,7 @@ input.onButtonPressed(Button.B, function () {
 // every 2 seconds it turns on pin 12 for 1 second
 // which turns on the sensor. pin 0 reads the sensor
 // and writes the data into the variable
-// moisture_reading. then pin 12 is turned off
+// moisture_reading. then pin 12 is turned off and the display function is called
 basic.forever(function () {
     while (Cal1 == 6) {
         //basic.clearScreen()
@@ -206,6 +205,7 @@ basic.forever(function () {
     }
 
 })
+//setting up variables
 let Cal1 = 0
 let lowReading3: number = 0
 let lowReading2: number = 0
@@ -216,6 +216,7 @@ let hiReading1: number = 0
 let hiAverage: number = 0
 let lowAverage: number = 0
 let moisture_reading: number = 0
+//calling setup function to do initial calibration. 
 setup()
 
 
